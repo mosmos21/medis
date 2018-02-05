5ab95e7b3b84989dbdcd94e367d1d64cb5c30808
ryota_shinzato <ryota_shinzato@unirita.co.jp>
2018-02-05 14:35:01 +0900

    ref #63 Angular Material追加

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatSidenavModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
