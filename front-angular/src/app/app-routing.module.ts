037a7c3a6cd4f2aeede847406e9e480d92a5c362
takahito hashimoto <takahito_hashimoto@unirita.co.jp>
2018-02-09 09:50:36 +0900

    ref #79: トップページを表示したときにログインしていなかった場合ログインページに遷移するようにした

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { TopComponent } from './top/top.component';
 import { LoginComponent } from './login/login.component';
 import { SelectTemplateComponent } from './select-template/select-template.component';
 import { EditTemplateComponent } from './edit-template/edit-template.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'top', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
-  { path: 'top', component: TopComponent },
+  { path: 'top', component: TopComponent, canActivate: [ AuthGuardService ]},
   { path: 'admin/template', component: SelectTemplateComponent},
   { path: 'admin/template/configure', component: EditTemplateComponent},
 ];
