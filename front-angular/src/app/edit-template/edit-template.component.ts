eaa8c64a0ccf06fbb8ab9c7580ab89ea49af3077
ryota_shinzato <ryota_shinzato@unirita.co.jp>
2018-02-19 10:51:03 +0900

    ref #99: タグ一覧をgetするようにした

--- front-angular/src/app/edit-template/edit-template.component.ts
+++ front-angular/src/app/edit-template/edit-template.component.ts
@@ -235,12 +235,14 @@ export class EditTemplateComponent implements OnInit {
   }
 
   addTags(event: any) {
-    if (this.selected_tags.length < 5) {
+    if (this.selected_tags.length < 4) {
       var str = event.path[0].innerHTML;
       this.selected_tags.push(str);
-      var idx = this.tags.indexOf(str);
-      if (idx >= 0) {
-        this.tags.splice(idx, 1);
+      var i = this.temp_tags.length;
+      while (i--) {
+        if (this.temp_tags[i] == str) {
+          this.temp_tags.splice(i, 1);
+        }
       }
     }
     this.seach_word = "";
@@ -248,8 +250,7 @@ export class EditTemplateComponent implements OnInit {
 
   deleteTags(event: any) {
     var str = event.path[0].innerHTML;
-    console.log(str);
-    this.tags.push(str);
+    this.temp_tags.push(str);
     var idx = this.selected_tags.indexOf(str);
     if (idx >= 0) {
       this.selected_tags.splice(idx, 1);
@@ -257,13 +258,15 @@ export class EditTemplateComponent implements OnInit {
   }
 
   searchTag() {
-    var targetTag = [];
-    var i = this.tags.length;
+    var i = this.temp_tags.length;
+    var str = this.seach_word;
+    var target_tags: any = [];
     while (i--) {
-      if (this.tags[i].indexOf(this.seach_word) > -1) {
-        targetTag.push(this.tags[i]);
+      if (this.temp_tags[i].indexOf(str) > -1) {
+        var temp_str = this.temp_tags[i];
+        target_tags.push(temp_str);
       }
     }
-    return targetTag;
+    return target_tags;
   }
 }
