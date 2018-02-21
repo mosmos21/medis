a0fdf4120c46358950cc7c5a5c42360d2c0b85c8
hirotaka sudo <hirotaka_sudo@unirita.co.jp>
2018-02-21 13:54:15 +0900

    notification_form tag_repository

--- medis/src/main/java/jp/co/unirita/medis/domain/tag/TagRepository.java
+++ medis/src/main/java/jp/co/unirita/medis/domain/tag/TagRepository.java
@@ -1,6 +1,13 @@
 package jp.co.unirita.medis.domain.tag;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface TagRepository extends JpaRepository<Tag, String> {
+	List<Tag> findByTagName(String tagName);
+	Tag findByTagId(String tagId);
+
 }
