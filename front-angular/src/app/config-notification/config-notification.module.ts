ea484658932c8d390d5126a4b19876973f5b7cca
ryota_shinzato <ryota_shinzato@unirita.co.jp>
2018-02-21 15:50:30 +0900

    ref#112: GETまで実装

--- front-angular/src/app/config-notification/config-notification.module.ts
+++ front-angular/src/app/config-notification/config-notification.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfigNotificationComponent } from './config-notification.component';
 import { ConfigNotificationRoutingModule } from './config-notification-routing.module';
+import {
+  MatButtonModule,
+  MatSlideToggleModule,
+} from '@angular/material';
 
 @NgModule({
-  imports: [ ConfigNotificationRoutingModule, CommonModule],
-  declarations: [ ConfigNotificationComponent ]
+  imports: [
+    ConfigNotificationRoutingModule,
+    CommonModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+  ],
+  declarations: [ConfigNotificationComponent]
 })
 export class ConfigNotificationModule { }
