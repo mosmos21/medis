5ba16d9983d64235673418cd71582259e55a21e5
takahito hashimoto <takahito_hashimoto@unirita.co.jp>
2018-02-23 09:19:12 +0900

    close #138: 主キーを定義、repositoryの定義の修正

--- medis/src/main/java/jp/co/unirita/medis/domain/tempkeyInfo/TempkeyInfo.java
+++ medis/src/main/java/jp/co/unirita/medis/domain/tempkeyInfo/TempkeyInfo.java
@@ -1,17 +1,24 @@
 package jp.co.unirita.medis.domain.tempkeyInfo;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@IdClass(TempkeyInfo.PK.class)
 @Table(name = "tempkey_info")
+@AllArgsConstructor
+@NoArgsConstructor
 public class TempkeyInfo {
 
 	@Id
@@ -23,4 +30,12 @@ public class TempkeyInfo {
 	private String tempKey;
 
 	private Timestamp changeDate;
+
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class PK implements Serializable {
+		private String employeeNumber;
+		private String tempKey;
+	}
 }
--- medis/src/main/java/jp/co/unirita/medis/domain/tempkeyInfo/TempkeyInfoRepository.java
+++ medis/src/main/java/jp/co/unirita/medis/domain/tempkeyInfo/TempkeyInfoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TempkeyInfoRepository extends JpaRepository<TempkeyInfo, String> {
+public interface TempkeyInfoRepository extends JpaRepository<TempkeyInfo, TempkeyInfo.PK> {
 	TempkeyInfo findByTempKey(String key);
 }
