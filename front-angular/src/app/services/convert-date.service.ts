6e269c8cbf7f4bbeffc6bafb4907f0c7f2906573
hiroya nakakawaji <hiroya_nakakawaji@unirita.co.jp>
2018-02-23 11:55:44 +0900

    上げなおし

--- front-angular/src/app/services/convert-date.service.ts
+++ front-angular/src/app/services/convert-date.service.ts
@@ -6,6 +6,16 @@ export class ConvertDateService {
   constructor() { }
 
   time2Date(time: number): string {
+    var d = new Date( time );
+    var year  = d.getFullYear();
+    var month = d.getMonth() + 1;
+    var day  = d.getDate();
+    var hour = ( d.getHours()   < 10 ) ? '0' + d.getHours()   : d.getHours();
+    var min  = ( d.getMinutes() < 10 ) ? '0' + d.getMinutes() : d.getMinutes();
+    return ( year + '/' + month + '/' + day + ' ' + hour + ':' + min );
+  }
+
+  time2DateSec(time: number): string {
     var d = new Date( time * 1000 );
     var year  = d.getFullYear();
     var month = d.getMonth() + 1;
