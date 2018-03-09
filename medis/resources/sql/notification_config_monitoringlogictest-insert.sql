058913b80882a2566730b2d460610a0ad87c1bb9
yuuki kagaya <yuuki_kagaya@unirita.co.jp>
2018-03-09 11:07:11 +0900

    ref #188: テストコード:setting:monitoringLogicTest

---
+++ medis/resources/sql/notification_config_monitoringlogictest-insert.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO "notification_config"
+     (employee_number, tag_id, mail_notification, browser_notification)
+VALUES
+    ('admin', 'n0000000000', 'true', 'true'),
+    ('admin', 's0000000000', 'false', 'false'),
+    ('g00000', 'n0000000001', 'true', 'false'),
+    ('g00000', 's0000000001', 'false', 'true');
