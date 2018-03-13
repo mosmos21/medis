fae0d2479b0c8b45bd406d5c2839e2788396dace
takahito hashimoto <takahito_hashimoto@unirita.co.jp>
2018-03-13 16:08:46 +0900

    close #267: 作成者を表示させる

--- medis/src/main/java/jp/co/unirita/medis/logic/setting/MonitoringLogic.java
+++ medis/src/main/java/jp/co/unirita/medis/logic/setting/MonitoringLogic.java
@@ -61,7 +61,7 @@ public class MonitoringLogic {
 	    List<DocumentInfo> documentInfoList2 = documentInfoRepository.findByDocumentIdIn(documentIdList);
 	    List<DocumentInfo> documentInfoList = Stream.concat(documentInfoList1.stream(), documentInfoList2.stream())
 	    		.distinct()
-	    		.sorted(Comparator.comparing(DocumentInfo::getDocumentCreateDate))
+	    		.sorted(Comparator.comparing(DocumentInfo::getDocumentCreateDate).reversed())
 	    		.collect(Collectors.toList());
 	    List<UserDetail> userDetail = new ArrayList<>();
 	    for (DocumentInfo docInfo : documentInfoList) {
@@ -73,110 +73,4 @@ public class MonitoringLogic {
 		}
 		return form;
 	}
-		/*
-		//userが監視しているタグの一覧
-		List<NotificationConfig> notificationConfig = notificationConfigRepository.findByEmployeeNumber(employeeNumber);
-		List<String> tagIdList = new ArrayList<>();
-
-		for (NotificationConfig tag : notificationConfig) {
-			tagIdList.add(tag.getTagId());
-		}
-
-		//文書についているタグが付いている文書の一覧
-		List<DocumentTag> documentTag = new ArrayList<>();
-
-		for (String ids : tagIdList) {
-			documentTag.addAll(documentTagRepository.findByTagId(ids));
-		}
-		List<String> documentList = new ArrayList<>();
-
-		for (DocumentTag docId : documentTag) {
-			documentList.add(docId.getDocumentId());
-		}
-
-		//テンプレートについているタグが付いている文書の一覧
-		List<TemplateTag> templateTag = new ArrayList<>();
-
-		for (String ids : tagIdList) {
-			templateTag.addAll(templateTagRepository.findByTagId(ids));
-		}
-
-		List<String> templateList = new ArrayList<>();
-
-		for (TemplateTag tempId : templateTag) {
-			templateList.add(tempId.getTemplateId());
-		}
-
-		//documentListのidのdocument_info一覧
-		List<DocumentInfo> docDocInfo = new ArrayList<>();
-
-		for (String docs : documentList) {
-			docDocInfo.addAll(documentInfoRepository.findByDocumentId(docs));
-		}
-
-		List<String> docDocInfoId = new ArrayList<>();
-
-		for (DocumentInfo info : docDocInfo) {
-			docDocInfoId.add(info.getDocumentId());
-		}
-
-		//templateListのidが付いているdocument_info一覧
-		List<DocumentInfo> tempDocInfo = new ArrayList<>();
-
-		for (int i = 0; i < templateList.size(); i++) {
-			tempDocInfo.addAll(documentInfoRepository.findByTemplateId(templateList.get(i)));
-		}
-
-		List<String> tempDocInfoId = new ArrayList<>();
-
-		for (DocumentInfo Info : tempDocInfo) {
-			tempDocInfoId.add(Info.getDocumentId());
-		}
-
-		//documentIdのマージ(値は重複している)
-		List<String> documentIdListBeforeMap = Stream.concat(docDocInfoId.stream(), tempDocInfoId.stream()).collect(Collectors.toList());
-
-		//値を一意に
-		Set<String> set = new HashSet<>(documentIdListBeforeMap);
-		List<String> documentIdList = new ArrayList<>(set);
-
-		//各documentIdごとの最新のupdateIdをもったupdate_infoのリストの取得
-		List<UpdateInfo> updateInfoList = new ArrayList<>();
-
-		for (String ids : documentIdList) {
-			updateInfoList.add(updateInfoRepository
-					.findFirstByDocumentIdAndUpdateTypeBetweenOrderByUpdateIdDesc(ids, TYPE_CREATE_DOCUMENT, TYPE_UPDATE_DOCUMENT));
-		}
-
-		//updateInfoListのdocumentIdの一覧の取得
-		List<String> updateDocIdList = new ArrayList<>();
-
-		for (UpdateInfo upDocId : updateInfoList) {
-			updateDocIdList.add(upDocId.getDocumentId());
-		}
-
-		//updateDocIdListのdocumentInfoの取得
-		List<DocumentInfo> documentInfoList = new ArrayList<>();
-
-		for (String ids : updateDocIdList) {
-			documentInfoList.addAll(documentInfoRepository.findByDocumentId(ids));
-		}
-
-		//updateDocIdListのuserDetailの取得
-		List<UserDetail> userDetail = new ArrayList<>();
-
-		for (String ids : updateDocIdList) {
-			userDetail.add(userDetailRepository.findOne(ids));
-		}
-
-		//documentInfoListとuserDetailの値をDocumentInfoFormに格納
-		List<DocumentInfoForm> form = new ArrayList<>();
-
-		for (int i = 0; i < documentInfoList.size(); i++) {
-			form.add(new DocumentInfoForm(documentInfoList.get(i), userDetail.get(i)));
-		}
-
-		form.sort(Comparator.comparing(DocumentInfoForm::getDocumentCreateDate).reversed());
-
-		return form;*/
 }
