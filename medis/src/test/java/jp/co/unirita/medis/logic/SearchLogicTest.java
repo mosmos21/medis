31d15c7bba6b49304b276d177c96dd573b5e8677
takahito hashimoto <takahito_hashimoto@unirita.co.jp>
2018-03-15 13:14:54 +0900

    fix #281: ファイル名調整

--- medis/src/test/java/jp/co/unirita/medis/logic/SearchLogicTest.java
+++ medis/src/test/java/jp/co/unirita/medis/logic/SearchLogicTest.java
@@ -22,10 +22,10 @@ import jp.co.unirita.medis.logic.util.SearchLogic;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Sql({ "file:resources/sql/document_info-delete.sql", "file:resources/sql/document_info-insert.sql",
-		"file:resources/sql/template_tag_monitoringlogictest-delete.sql",
-		"file:resources/sql/template_tag_monitoringlogictest-insert.sql",
-		"file:resources/sql/document_tag_monitoringlogictest-delete.sql",
-		"file:resources/sql/document_tag_monitoringlogictest-insert.sql",
+		"file:resources/sql/template_tag_monitoring_logic_test-delete.sql",
+		"file:resources/sql/template_tag_monitoring_logic_test-insert.sql",
+		"file:resources/sql/document_tag-delete.sql",
+		"file:resources/sql/document_tag_monitoring_logic_test-insert.sql",
 		"file:resources/sql/tag-delete.sql","file:resources/sql/tag-insert.sql" })
 public class SearchLogicTest {
 
