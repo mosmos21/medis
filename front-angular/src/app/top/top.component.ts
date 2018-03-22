7b9d44a14141a133ceccd5eaa21c91ac37f3a704
takahito hashimoto <takahito_hashimoto@unirita.co.jp>
2018-03-22 11:22:19 +0900

    close #327: httpリクエストを修正、インターフェースを作成

--- front-angular/src/app/top/top.component.ts
+++ front-angular/src/app/top/top.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router'
+import { Component, OnInit, Inject } from '@angular/core';
 
 import { AuthService } from '../services/auth.service';
-import { CookieService } from 'ngx-cookie-service'
+import { HttpService } from '../services/http.service';
 import { ErrorService } from '../services/error.service';
-import { ConvertDateService } from '../services/convert-date.service';
-import { NavigationService } from '../services/navigation.service';
 import { IsNewService } from '../services/is-new.service';
+import { CookieService } from 'ngx-cookie-service'
+import { NavigationService } from '../services/navigation.service';
+import { ConvertDateService } from '../services/convert-date.service';
+
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-top',
@@ -16,72 +18,63 @@ import { IsNewService } from '../services/is-new.service';
 })
 export class TopComponent implements OnInit {
 
-  public updateList: any;
-  public ownDocList: any;
-  public favDocList: any;
-  public monDocList: any;
+  public updateList: DocumentInfo[] = new Array();
+  public ownDocList: DocumentInfo[] = new Array();
+  public favDocList: DocumentInfo[] = new Array();
+  public monDocList: DocumentInfo[] = new Array();
 
-  private user: any;
+  private user: User;
 
   constructor(
-    private http: HttpClient,
-    @Inject('hostname') private hostname: string,
-    private router: Router,
-    private nav: NavigationService,
-    private authService: AuthService,
-    private cookieService: CookieService,
-    private errorService: ErrorService,
     public conv: ConvertDateService,
     public isNewService: IsNewService,
+    private nav: NavigationService,
+    private http: HttpService,
+    private router: Router,
+    private authService: AuthService,
+    private errorService: ErrorService,
   ) {
     this.nav.show();
     this.nav.showUserMenu();
     this.user = this.authService.user;
     this.authService.getUserDetail();
-    // console.log(this.user);
   }
 
   ngOnInit() {
-    this.http.get(this.hostname + "infomations", { headers: this.authService.headerAddToken(), withCredentials: true }).subscribe(
-      json => {
-        this.updateList = json;
-        // console.log(this.updateList);
-      },
-      error => {
-        this.errorService.errorPath(error.status)
-      }
-    );
-    this.http.get(this.hostname + "documents/public", { headers: this.authService.headerAddToken(), withCredentials: true }).subscribe(
-      json => {
-        this.ownDocList = json;
-        // console.log(this.updateList);
-      },
-      error => {
-        this.errorService.errorPath(error.status)
-      }
-    );
-    this.http.get(this.hostname + "documents/bookmark", { headers: this.authService.headerAddToken(), withCredentials: true }).subscribe(
-      json => {
-        this.favDocList = json;
-        // console.log(this.updateList);
-      },
-      error => {
-        this.errorService.errorPath(error.status)
-      }
-    );
-    this.http.get(this.hostname + "documents/monitoring_tags", { headers: this.authService.headerAddToken(), withCredentials: true }).subscribe(
-      json => {
-        this.monDocList = json;
-        // console.log(this.updateList);
-      },
-      error => {
-        this.errorService.errorPath(error.status)
-      }
-    );
+    this.http.get("infomations").subscribe(list => {
+      this.updateList = list;
+    }, error => {
+      this.errorService.errorPath(error.status);
+    });
+    this.http.get("documents/public").subscribe(list => {
+      this.ownDocList = list;
+    }, error => {
+      this.errorService.errorPath(error.status);
+    });
+    this.http.get("documents/bookmark").subscribe(list => {
+      this.favDocList = list;
+    }, error => {
+      this.errorService.errorPath(error.status);
+    });
+    this.http.get("documents/monitoring_tags").subscribe(list => {
+      this.monDocList = list;
+    }, error => {
+      this.errorService.errorPath(error.status);
+    });
   }
 
-  viewDocument(id: string) {
+  viewDocument(id: string): void {
     this.router.navigate(["browsing/" + id]);
   }
+}
 
+interface DocumentInfo {
+  documentCreateDate: string,
+  documentId: string,
+  documentName: string,
+  documentPublish: boolean,
+  employeeNumber: string,
+  firstName: string,
+  lastName: string,
+  templateId: string,
 }
