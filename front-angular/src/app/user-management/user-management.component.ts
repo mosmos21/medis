2e478c32a4dd3cc98e554944a0658814ee8cd5db
takahito hashimoto <takahito_hashimoto@unirita.co.jp>
2018-03-22 11:00:36 +0900

    close #323: フォームクラスを使用したロジックに修正、細かいロジックを修正

--- front-angular/src/app/user-management/user-management.component.ts
+++ front-angular/src/app/user-management/user-management.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, Inject, HostListener } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MatDialog } from '@angular/material';
 
-import { ConfirmationComponent } from '../confirmation/confirmation.component';
 import { CreateUserComponent } from '../create-user/create-user.component'
+import { ConfirmationComponent } from '../confirmation/confirmation.component';
 import { MessageModalComponent } from '../message-modal/message-modal.component'
 import { InitializationComponent } from '../initialization/initialization.component'
+
 import { AuthService } from '../services/auth.service';
-import { NavigationService } from '../services/navigation.service';
+import { HttpService } from '../services/http.service';
 import { ErrorService } from '../services/error.service';
 import { SnackBarService } from '../services/snack-bar.service';
+import { NavigationService } from '../services/navigation.service';
 
 @Component({
   selector: 'app-user-management',
@@ -18,30 +19,15 @@ import { SnackBarService } from '../services/snack-bar.service';
 })
 export class UserManagementComponent implements OnInit {
 
-  private users: any = [{
-    employeeNumber: '',
-    lastName: '',
-    firstName: '',
-    lastNamePhonetic: '',
-    firstNamePhonetic: '',
-    mailaddress: '',
-    icon: false,
-    authorityId: '',
-    enabled: true
-  }];
-  private enable;
-  private user;
-  public searchWord = "";
-
-  private message;
+  public searchWord: string = '';
+  private users: UserForm[] = new Array();
 
   constructor(
-    private http: HttpClient,
-    @Inject('hostname') private hostname: string,
+    public nav: NavigationService,
     public dialog: MatDialog,
+    private http: HttpService,
     private authService: AuthService,
     private errorService: ErrorService,
-    public nav: NavigationService,
     private snackBarService: SnackBarService,
   ) {
     this.nav.showAdminMenu();
@@ -50,140 +36,102 @@ export class UserManagementComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.http.get(this.hostname + 'system/users', { withCredentials: true, headers: this.authService.headerAddToken() }).subscribe(
-      json => {
-        this.users = json;
-      },
-      error => {
-        this.errorService.errorPath(error.status)
-      }
-    );
+    this.http.get('system/users').subscribe(users => {
+      this.users = users;
+    }, error => {
+      this.errorService.errorPath(error.status);
+    });
   }
 
   confirmChangeEnable(e: any, index: number): void {
     e.preventDefault();
-
-    if (this.users[index]["enabled"]) {
-      this.enable = "無効";
-    } else {
-      this.enable = "有効";
-    }
-
-    this.message = this.users[index]['lastName'] + this.users[index]['firstName'] + 'さんのアカウントを' + this.enable + '化します。'
-
-    console.log(this.message)
-
-    let dialogRef = this.dialog.open(ConfirmationComponent, {
+    const enable = this.users[index].enabled ? '無効' : '有効';
+    const dialogRef = this.dialog.open(ConfirmationComponent, {
       data: {
-        message: this.message
+        message: this.users[index].lastName + this.users[index].firstName + 'さんのアカウントを' + enable + '化します。'
       }
     });
-
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.users[index]["enabled"] = !this.users[index]["enabled"];
-        this.snackBarService.openSnackBar("変更しました", "");
+        this.users[index].enabled = !this.users[index].enabled;
+        this.snackBarService.openSnackBar('変更しました', '');
       }
     });
   }
 
   initialization(index: number): void {
-
-    this.message = this.users[index]['lastName'] + this.users[index]['firstName'] + 'さんにパスワード初期化用メールを送信しました。'
-
-    this.user = {
-      employeeNumber: this.users[index]["employeeNumber"],
-      mailaddress: this.users[index]["mailaddress"]
+    const data = {
+      employeeNumber: this.users[index].employeeNumber,
+      mailaddress: this.users[index].mailaddress,
+      password: 'DUMMY',
     }
 
-    let dialogRef = this.dialog.open(InitializationComponent, {
+    const dialogRef = this.dialog.open(InitializationComponent, {
       data: {
         user: this.users[index],
       }
     });
-
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
       if (result != null) {
-        this.user['password'] = 'dummypass'
-        this.http.post(this.hostname + "accounts/usercheck", this.user, { withCredentials: true, headers: this.authService.headerAddToken() }).subscribe(
-          json => { },
-          error => {
-            this.errorService.errorPath(error.status);
-          }
-        );
-        this.snackBarService.openSnackBar(this.message, "");
+        this.http.post('accounts/usercheck', data).subscribe(success => {
+        }, error => {
+          this.errorService.errorPath(error.status);
+        });
+        this.snackBarService.openSnackBar(this.users[index].lastName + this.users[index].firstName + 'さんにパスワード初期化用メールを送信しました。', '');
       }
     });
   }
 
   createUser(): void {
-
-    this.user = {
-      employeeNumber: '',
-      lastName: '',
-      firstName: '',
-      lastNamePhonetic: '',
-      firstNamePhonetic: '',
-      mailaddress: '',
-      icon: false,
-      authorityId: '',
-      enabled: true
-    }
-
-    let dialogRef = this.dialog.open(CreateUserComponent, {
+    let user: UserForm = new UserForm();
+    const dialogRef = this.dialog.open(CreateUserComponent, {
       data: {
-        user: this.user
+        user: user,
       }
     });
-
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
       if (result != null) {
         this.users.push(result);
-        this.http.post(this.hostname + "system/users/new", this.user, { withCredentials: true, headers: this.authService.headerAddToken() }).subscribe(
-          json => {
-            console.log("success:");
-            console.log(json);
-          },
-          error => {
-            console.log("error:");
-            console.log(error);
-            this.errorService.errorPath(error.status)
-          }
-        );
+        this.http.post('system/users/new', user).subscribe(success => {
+        }, error => {
+          this.errorService.errorPath(error.status);
+        });
       }
     });
   }
 
-  changeEnable(index: number) {
-    this.users[index]["enabled"] = !this.users[index]["enabled"];
+  changeEnable(index: number): void {
+    this.users[index].enabled = !this.users[index].enabled;
   }
 
-  searchUser() {
-    var sub = JSON.stringify(this.users);
-    var targetUser = JSON.parse(sub);
-    var i = targetUser.length;
-    var word = this.searchWord;
-    while (i--) {
-      if (
-        (String(this.users[i]["employeeNumber"]).indexOf(word) == -1) &&
-        (this.users[i]["lastName"].indexOf(word) == -1) &&
-        (this.users[i]["firstName"].indexOf(word) == -1) &&
-        (this.users[i]["lastNamePhonetic"].indexOf(word) == -1) &&
-        (this.users[i]["firstNamePhonetic"].indexOf(word) == -1) &&
-        (this.users[i]["mailaddress"].indexOf(word) == -1)
-      ) {
-        targetUser.splice(i, 1);
-      }
-    }
-    return targetUser;
+  searchUser(): UserForm[] {
+    return this.users.filter(user =>
+      user.employeeNumber.includes(this.searchWord)
+      || user.lastName.includes(this.searchWord)
+      || user.firstName.includes(this.searchWord)
+      || user.lastNamePhonetic.includes(this.searchWord)
+      || user.firstNamePhonetic.includes(this.searchWord)
+      || user.mailaddress.includes(this.searchWord)
+    );
   }
 
   @HostListener('window:unload', ['$event'])
   unloadHandler() {
-    this.http.post(this.hostname + "users/update", this.users, { withCredentials: true, headers: this.authService.headerAddToken() }).subscribe(
-      /* postした時の操作があればここにかく */
-    );
+    this.http.post('users/update', this.users).subscribe(success => {
+    }, error => {
+      this.errorService.errorPath(error.status);
+    });
   }
+}
+
+class UserForm {
+  public employeeNumber: string;
+  public lastName: string;
+  public firstName: string;
+  public lastNamePhonetic: string;
+  public firstNamePhonetic: string;
+  public mailaddress: string;
+  public icon: boolean;
+  public authorityId: string;
+  public enabled: boolean;
 }
