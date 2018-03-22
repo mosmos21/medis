ec0ab07e5b0a079107656a3f937a9a663e88216e
takahito hashimoto <takahito_hashimoto@unirita.co.jp>
2018-03-22 13:48:03 +0900

    close #331: fix

--- front-angular/src/app/login/login.component.ts
+++ front-angular/src/app/login/login.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MatDialog } from '@angular/material';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, Inject } from '@angular/core';
 
 import { ResetPassComponent } from '../reset-pass/reset-pass.component';
 import { MessageModalComponent } from '../message-modal/message-modal.component';
 
-import { NavigationService } from '../services/navigation.service';
 import { AuthService } from '../services/auth.service';
-import { ValidatorService } from '../services/validator.service';
+import { HttpService } from '../services/http.service';
 import { SnackBarService } from '../services/snack-bar.service';
-import { User } from '../model/User';
+import { ValidatorService } from '../services/validator.service';
+import { NavigationService } from '../services/navigation.service';
 
+import { User } from '../model/User';
 import { reject } from 'q';
 import { resolve } from 'url';
-import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-login',
@@ -28,52 +28,20 @@ export class LoginComponent implements OnInit {
   public password: string = 'pass2017';
   public hide: boolean;
 
-  private message: string;
   private user: User;
 
   constructor(
-    public authService: AuthService,
     public router: Router,
     public dialog: MatDialog,
+    public authService: AuthService,
     public snackBarService: SnackBarService,
     private nav: NavigationService,
-    private valid: ValidatorService,
     private http: HttpService,
+    private valid: ValidatorService,
   ) {
     this.nav.hide();
   }
 
-  openDialog(): void {
-    let dialogRef = this.dialog.open(ResetPassComponent, {
-      width: '400px',
-      data: { user: this.user },
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
-      if (result != null) {
-        this.http.post("accounts/usercheck", this.user).subscribe(
-          success => {
-            this.message = 'パスワード再設定用メールを送信しました。'
-            let dialogRef = this.dialog.open(MessageModalComponent, {
-              data: {
-                message: this.message
-              }
-            });
-          },
-          error => {
-            console.log(error);
-            this.message = '入力された社員番号とメールアドレスが不正です。'
-            let dialogRef = this.dialog.open(MessageModalComponent, {
-              data: {
-                message: this.message
-              }
-            });
-          }
-        );
-      }
-    });
-  }
 
   ngOnInit() { }
 
@@ -85,8 +53,32 @@ export class LoginComponent implements OnInit {
     if (!this.valid.empty(loginUser)) {
       this.login();
     } else {
-      this.errorMessage = "入力必須項目が未入力です。";
+      this.errorMessage = '入力必須項目が未入力です。';
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ResetPassComponent, {
+      width: '400px',
+      data: { user: this.user },
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result != null) {
+        this.http.post('accounts/usercheck', this.user).subscribe(success => {
+          let dialogRef = this.dialog.open(MessageModalComponent, {
+            data: {
+              message: 'パスワード再設定用メールを送信しました。'
+            }
+          });
+        }, error => {
+          let dialogRef = this.dialog.open(MessageModalComponent, {
+            data: {
+              message: '入力された社員番号とメールアドレスが不正です。'
+            }
+          });
+        });
+      }
+    });
   }
 
   login() {
@@ -94,7 +86,7 @@ export class LoginComponent implements OnInit {
       if (this.authService.isLoggedIn()) {
         let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : 'top';
         this.router.navigate([redirect]);
-        this.snackBarService.openSnackBar("ログインしました", "");
+        this.snackBarService.openSnackBar('ログインしました', '');
       } else {
         this.errorMessage = this.authService.message;
       }
