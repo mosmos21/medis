05c53721e7ac522dfb9ebab9ececd81a9c43e0f2
ryota_shinzato <ryota_shinzato@unirita.co.jp>
2018-03-22 16:32:47 +0900

    close #341: フリガナを全角カナ固定に

--- medis/src/main/resources/data.sql
+++ medis/src/main/resources/data.sql
@@ -24,14 +24,14 @@ DELETE FROM "user_detail";
 INSERT INTO "user_detail"
     (employee_number, first_name, first_name_phonetic, icon, last_name, last_name_phonetic, mailaddress)
 VALUES
-    ('admin', '者', 'しゃ', false, '管理', 'かんり', 'medis.masa0@gmail.com'),
-    ('g00000', '太郎', 'たろう', false, 'ユニリタ', 'ゆにりた', 'medis.masa0@gmail.com'),
-    ('97968', '尚儒', 'たかひと', false, '橋本', 'はしもと', 'takahito_hashimoto@unirita.co.jp'),
-    ('97958', '雅則', 'まさのり', false, '浅野', 'あさの', 'masanori_asano@unirita.co.jp'),
-    ('97965', '亮太', 'りょうた', false, '新里', 'しんざと', 'ryota_shinzato@unirita.co.jp'),
-    ('97966', '宏崇', 'ひろたか', false, '須藤', 'すどう', 'hirotaka_sudo@unirita.co.jp'),
-    ('97967', '玄哉', 'ひろや', false, '中川路', 'なかかわじ', 'hiroya_nakakawaji@unirita.co.jp'),
-    ('97962', '優輝', 'ゆうき', false, '加賀谷', 'かがや', 'yuuki_kagaya@unirita.co.jp');
+    ('admin', '者', 'シャ', false, '管理', 'カンリ', 'medis.masa0@gmail.com'),
+    ('g00000', '太郎', 'タロウ', false, 'ユニリタ', 'ユニリタ', 'medis.masa0@gmail.com'),
+    ('97968', '尚儒', 'タカヒト', false, '橋本', 'ハシモト', 'takahito_hashimoto@unirita.co.jp'),
+    ('97958', '雅則', 'マサノリ', false, '浅野', 'アサノ', 'masanori_asano@unirita.co.jp'),
+    ('97965', '亮太', 'リョウタ', false, '新里', 'シンザト', 'ryota_shinzato@unirita.co.jp'),
+    ('97966', '宏崇', 'ヒロタカ', false, '須藤', 'スドウ', 'hirotaka_sudo@unirita.co.jp'),
+    ('97967', '玄哉', 'ヒロヤ', false, '中川路', 'ナカカワジ', 'hiroya_nakakawaji@unirita.co.jp'),
+    ('97962', '優輝', 'ユウキ', false, '加賀谷', 'カガヤ', 'yuuki_kagaya@unirita.co.jp');
 
 
 DELETE FROM "tag";
