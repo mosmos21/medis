f4891dcfd7ccb79c2ba80c6a7ebc5f7eefc02a45
takahito hashimoto <takahito_hashimoto@unirita.co.jp>
2018-03-26 09:35:06 +0900

    ref #356: settingLogicの呼び出しを移動

--- medis/src/main/java/jp/co/unirita/medis/MedisApplication.java
+++ medis/src/main/java/jp/co/unirita/medis/MedisApplication.java
@@ -1,12 +1,25 @@
 package jp.co.unirita.medis;
 
+import jp.co.unirita.medis.logic.setting.SettingLogic;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class MedisApplication {
+public class MedisApplication implements CommandLineRunner {
+
+	@Resource
+	SettingLogic settingLogic;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedisApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+		settingLogic.deleteAll();
+		settingLogic.init();
 	}
 }
--- medis/src/main/java/jp/co/unirita/medis/config/SpringBootUploadFileApplication.java
+++ medis/src/main/java/jp/co/unirita/medis/config/SpringBootUploadFileApplication.java
@@ -7,20 +7,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import jp.co.unirita.medis.logic.setting.SettingLogic;
-
-@SpringBootApplication
-public class SpringBootUploadFileApplication implements CommandLineRunner {
-
-	@Resource
-	SettingLogic settingLogic;
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootUploadFileApplication.class, args);
-	}
-
-	@Override
-	public void run(String... arg) throws Exception {
-		settingLogic.deleteAll();
-		settingLogic.init();
-	}
-}
+//
+//@SpringBootApplication
+//public class SpringBootUploadFileApplication implements CommandLineRunner {
+//
+//	@Resource
+//	SettingLogic settingLogic;
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringBootUploadFileApplication.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... arg) throws Exception {
+//		settingLogic.deleteAll();
+//		settingLogic.init();
+//	}
+//}
